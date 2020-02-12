126e1ffffd64520299f84fe00e47d9a4c9bddccf
sas7359 <sas7359@g.rit.edu>
2020-02-12 09:36:11 -0500

    Added search bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Form, Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Form>
+          <Form.Group controlId="ingredientsForSearch">
+            <Form.Label>Search</Form.Label>
+            <Form.Control type="ingredients" placeholder="Enter ingredients.." />
+          </Form.Group>
+        </Form>
+      </Container>
     </div>
   );
 }
