24e4b331d7617de7c0495c2c6b05d9cdb9211da7
sas7359 <sas7359@g.rit.edu>
2020-02-20 10:43:58 -0500

    Updates

---
+++ precache-manifest.0e4fa3ed6c6c38cc6de3c2f10b1836ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35a18dcbbcac3294281f57a9a06a0c8d",
+    "url": "/RecipeProject/index.html"
+  },
+  {
+    "revision": "55b126025eab89649c71",
+    "url": "/RecipeProject/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "69097a9abdc72cb67899",
+    "url": "/RecipeProject/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "55b126025eab89649c71",
+    "url": "/RecipeProject/static/js/2.d49ba38a.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/RecipeProject/static/js/2.d49ba38a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69097a9abdc72cb67899",
+    "url": "/RecipeProject/static/js/main.df415bb3.chunk.js"
+  },
+  {
+    "revision": "8a48a896c13e8365a5ff",
+    "url": "/RecipeProject/static/js/runtime-main.eec7e4b6.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/RecipeProject/precache-manifest.0e4fa3ed6c6c38cc6de3c2f10b1836ae.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RecipeProject/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
