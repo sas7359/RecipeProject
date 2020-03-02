aedef2271da32cb540759b58dab7980d02bc2a61
Jack Li <jl1429@rit.edu>
2020-03-01 23:40:52 -0500

    still need to actually format the return from the api call.

--- src/js/RecipeModal.js
+++ src/js/RecipeModal.js
@@ -3,7 +3,21 @@ import Modal from 'react-bootstrap/Modal';
 import {Button} from 'react-bootstrap';
 
 export default class RecipeModal extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            recipe: []
+        }
+    }
 
+    getRecipe(id){
+        fetch("https://api.spoonacular.com/recipes/" + id
+        + "/analyzedInstructions?apiKey=053c403cb3f34a599d8b22c9afa1293b")
+            .then(res => res.json())
+            .then(data => {
+                this.setState({recipe: data});
+            })
+    }
     render(){
         return(
             <Modal {...this.props}>
@@ -14,6 +28,7 @@ export default class RecipeModal extends React.Component{
               </Modal.Header>
               <Modal.Body>
                 {this.props.modalId}
+                {this.getRecipe(this.props.modalId)}
               </Modal.Body>
               <Modal.Footer>
                 <Button variant="primary" onClick={this.props.onHide}>
